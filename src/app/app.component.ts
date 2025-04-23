1ce39de2b48b759c2fe6975e48e07f27665b13ae
karthik burra <kburra193@gmail.com>
2025-04-23 01:09:15 -0500

    initial stage setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet],  // Include RouterOutlet if you're using routing
+  templateUrl: './app.component.html',  // Your component template
+  styleUrls: ['./app.component.scss'],  // Your component styles
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]  // Enable the use of custom elements like <qlik-embed>
 })
 export class AppComponent {
   title = 'churn-insights-app';

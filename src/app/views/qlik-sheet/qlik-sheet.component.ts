81c6b73f3726f0297440c2f9e06c940446a90643
karthik burra <kburra193@gmail.com>
2025-04-23 01:42:27 -0500

    tab 1 entire app or sheet

--- src/app/views/qlik-sheet/qlik-sheet.component.ts
+++ src/app/views/qlik-sheet/qlik-sheet.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-qlik-sheet',
   imports: [],
   templateUrl: './qlik-sheet.component.html',
-  styleUrl: './qlik-sheet.component.scss'
+  styleUrl: './qlik-sheet.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]  // Enable the use of custom elements like <qlik-embed>
 })
 export class QlikSheetComponent {
 
